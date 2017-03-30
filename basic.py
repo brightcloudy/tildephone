ca79de57cfcec0590e3814b00029a3c27c98ad87
Ryan Karl <ryan@brightcloud.design>
2017-03-29 21:49:56 -0400

    Change from manually building the response to using the twilio package's twiml helper. Switch to working with POST requests and fetch some of the request info from Twilio.

--- basic.py
+++ basic.py
@@ -1,24 +1,17 @@
-from flask import Flask, Response
-from xmltodict import unparse
+from flask import Flask, request, Response
 from datetime import datetime
-from collections import OrderedDict
+from twilio import twiml
 app = Flask(__name__)
 
-@app.route('/voice.xml')
+@app.route('/voice.xml', methods=['GET', 'POST'])
 def basic_twiml():
     now = datetime.now()
-    twiml = OrderedDict([
-        ('Response', OrderedDict([
-            ('Pause', {
-                '@length': '2'
-                }),
-            ('Say', {
-                '#text': 'Hello there! It is now {0:%H} {0:%M} {0:%p}.'.format(now),
-                '@voice': 'woman'
-                })
-            ]))
-        ])
+    resp = twiml.Response()
+    if request.values.get('CallSid') == None:
+        return Response(response=str(resp), status=200, mimetype='text/xml')
 
-    xml = unparse(twiml)
-    resp = Response(response=xml, status=200, mimetype='text/xml')
-    return resp
+    resp.pause(length=2)
+    resp.say('Thank you for calling this beautiful telephone number. Your phone number is {}'.format(request.values.get('From')), voice='man')
+    resp.pause(length=5)
+    resp.say(request.values.get('CallSid'), voice='woman')
+    return Response(response=str(resp), status=200, mimetype='text/xml')
